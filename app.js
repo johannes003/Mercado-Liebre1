101e68bc80df462ee9045865403c963ef9b0917a
johannes003 <johannesandres03@gmail.com>
2024-02-27 22:14:35 -0500

    hola

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.listen(port, () => {
     console.log(`servidor corriendo en puerto ${port}`)
   });
 
-  app.get('/home', (req, res) =>{res.sendFile(path.resolve(__dirname, './view/home.html'));});
+  app.get('/', (req, res) =>{res.sendFile(path.resolve(__dirname, './view/home.html'));});
   app.get('/register', (req, res) =>{res.sendFile(path.resolve(__dirname, './view/register.html'));});
   app.get('/login', (req, res) =>{res.sendFile(path.resolve(__dirname, './view/login.html'));});
