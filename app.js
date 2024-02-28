c5c04ec2d51019dcc196c660e5f9f5a4a272ddde
johannes003 <johannesandres03@gmail.com>
2024-02-27 21:14:57 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('public'));
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log(`servidor corriendo en puerto ${port}`)
+  });
+
+  app.get('/home', (req, res) =>{res.sendFile(path.resolve(__dirname, './view/home.html'));});
+  app.get('/register', (req, res) =>{res.sendFile(path.resolve(__dirname, './view/register.html'));});
+  app.get('/login', (req, res) =>{res.sendFile(path.resolve(__dirname, './view/login.html'));});
